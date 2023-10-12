b53e11d6553742321d1372a481b61d0d0e2fc53f
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-12 12:09:55 +0300

    Added task 3 update 1

---
+++ models/base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""This module hosts one class model BaseModel.
+"""
+
+class BaseModel:
+     """This class model defines all common attributes,
+        methods for other classes.
+
+    Attributes:
+        id (str): A string-assign with uuid when instance is created.
+        created_at (datetime): Assigned with the current datetime.
+            when an instance is created.
+        updated_at (datetime): Assign with the current datetime
+            when instance is created.
+            Will be updated anytime the object is changed.
+
+    Public instance methods:
+        save(self): Updates the 'updated_at' attr with the current datetime.
+        to_dict(self): Returns dict containing all instance attributes.
+            - set using __dict__
+            - Set as the first step to serialization/deserialization process.
+    """
+import uuid
+from datetime import datetime
+
+    def __init__(self):
+        """Initializes a BaseModel instance with the given dict representation.
+
+        Args:
+        **kwargs: A dictionary with attributes and their values.
+        """
+    self.id = str(uuid.uuid4())
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+
+    def save(self):
+        """Updates the public instance attr updated_at with current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dict containing all keys/values of dict of the instance.
+        """
+
+        self.obj_dict = self.__dict__.copy()
+        self.obj_dict["__class__"] = self.__class__.__name__
+        self.obj_dict["created_at"] = self.created_at.isoformat()
+        self.obj_dict["updated_at"] = self.updated_at.isoformat()
+
+
+    def __str__(self):
+        """Print the class name, instance id and class dict representation.
+        """
+        return ([self.__class__.__name__] (self.id) self.__dict__)
+
+    
