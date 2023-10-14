7c37e6487115ddcd281c376eaca270592bb66e46
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-15 02:31:12 +0300

    Edited base_model.py for corrections and bugs.

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@
 
 import uuid
 from datetime import datetime
-#from models import storage
 
 
 class BaseModel:
@@ -33,9 +32,9 @@ class BaseModel:
         Args:
         **kwargs: A dictionary with attributes and their values.
         """
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """Updates the public instance attr updated_at with current datetime.
