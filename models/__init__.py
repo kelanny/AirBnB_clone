62f6df0c962b3e2cf2abf7fc03325b2044c5a398
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-15 02:29:20 +0300

    Edited base_model.py for corrections and bugs.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 """ __init__.py file for models directory """
 
-from models.engine import file_storage
-
-storage = file_storage.FileStorage()
-storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage
+#from models import storage
 
 
 class BaseModel:
@@ -33,24 +33,14 @@ class BaseModel:
         Args:
         **kwargs: A dictionary with attributes and their values.
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key in ['created_at', 'updated_at']:
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def save(self):
         """Updates the public instance attr updated_at with current datetime.
         """
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """returns a dict containing all keys/values of dict of the instance.
