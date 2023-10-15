aae7d336e46c5c6752ef821d105d76f880e6d88d
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-10-15 03:00:39 +0300

    Added task 4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -32,9 +32,19 @@ class BaseModel:
         Args:
         **kwargs: A dictionary with attributes and their values.
         """ 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Updates the public instance attr updated_at with current datetime.
